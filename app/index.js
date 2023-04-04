b449e65488a5febe3381a2ad1da31f08399d1d24
PraveenKoranga <praveenkoranga098@gmail.com>
2023-04-04 22:53:50 +0530

    structure of project setup

--- app/index.js
+++ app/index.js
@@ -1,6 +1,27 @@
-import { View, Text } from "react-native-web";
+import { Stack, useRouter } from "expo-router";
+import { View, Text, SafeAreaView } from "react-native";
+import { COLORS, icons } from "../constants";
+import { ScreenHeaderBtn } from "../components";
 
 const Home = () => {
-  return <View>Home</View>;
+  const router = useRouter();
+
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn iconUrl={icons.menu} dimensions="60%" />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn iconUrl={icons.menu} dimensions="60%" />
+          ),
+          headerTitle: "",
+        }}
+      />
+    </SafeAreaView>
+  );
 };
 export default Home;
